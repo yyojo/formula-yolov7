5818b80f8dbf475bb282b5a7aaf1c0be2ed40c58
Yoav Yosif Or <109002901+yyojo@users.noreply.github.com>
2022-10-25 23:26:36 +0300

    Add files via upload

---
+++ YoloV7/common_consts.py
@@ -0,0 +1,18 @@
+#tvt split
+DATASET_SPLIT_RATIO = {"train":0.75,"val":0.15,"test":0.1}
+
+#source path consts
+SRC_PATH = "../dataset/"
+IMAGE_SUB_FOLDER = "img"
+LABELS_SUB_FOLDER = "ann"
+
+#source json consts
+OBJECTS = "objects"
+CLASS_TITLE = "classTitle"
+POINTS = "points"
+EXT = "exterior"
+SIZE = "size"
+WIDTH = "width"
+HEIGHT = "height"
+
+IMG_EXTEN = ".jpg"
